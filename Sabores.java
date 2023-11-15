3284b4e8aa5c6f8678148731e68c5f8494b13a63
Felipe Perlato <fe.perlato@gmail.com>
2023-11-15 17:19:19 -0300

    termino exerc

---
+++ Sabores.java
@@ -0,0 +1,13 @@
+package Restaurante;
+import java.util.Scanner;
+public enum Sabores {
+	
+	 PRATOCASA,
+     PRATOMEDIANO,
+     PRATOSIMLES,
+     PRATOGG,
+     COCACOLA2L,
+     CERVEJA,
+     VINHO,
+     AGUACOMGAS;
+}
